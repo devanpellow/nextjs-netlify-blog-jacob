12a7f85f775c0707c76fbb123ab1a388a730b6b4
Devan Pellow <devanpellow@gmail.com>
2023-02-21 23:34:21 +0100

    Fis margin and add contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -20,6 +20,7 @@ export default function Contact() {
 					<SocialList />
 					<div className="contact-container">
 						<form name="contact" method="POST" data-netlify="true">
+                        <input type="hidden" name="form-name" value="contact" />
 							<div className="form-group">
 								<input
 									type="text"
